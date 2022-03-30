59f54b78004c823c6e081f6a29d5a0a04b965597
Jorge Marino <jamsoul@gmail.com>
2022-03-30 17:06:41 -0300

    working contract calling alt_bn128_add

--- altbn128/src/altbn128.cpp
+++ altbn128/src/altbn128.cpp
@@ -1,13 +1,17 @@
 #include <altbn128.hpp>
 
+extern "C" {
+
 __attribute__((eosio_wasm_import)) 
 void alt_bn128_add( const char* op1_data, uint32_t op1_length,
                     const char* op2_data, uint32_t op2_length, 
-                    const char* result );
+                    const char* result , uint32_t result_length);
+
+}
 
 ACTION altbn128::hi( name nm ) {
    /* fill in action body */
    print_f("Name : %\n",nm);
    char ret[1000];
-   alt_bn128_add( "hello", 5, "bye", 3, ret);
+   alt_bn128_add( "hello", 5, "bye", 3, ret, 1000);
 }
